88ec2deceb99db7f9febd9563330b8f70aa20e46
louJ-dev <jericfactor@gmail.com>
2026-04-03 19:41:22 +0800

    added cors

--- BMIS.backend/Program.cs
+++ BMIS.backend/Program.cs
@@ -1,9 +1,30 @@
 using BMIS.Endpoints;
 
+var ELECTRON_CORS = "electronCors";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/* 
+ * [!] WARNING
+ * 
+ * Generally UNSAFE to use "AllowAny*" options especially AllowAnyOrigin()
+ * Specific origins should be used
+ * In this case it should be electron frontend origin
+ *
+ * But bacause the project is local, I believe it is okay
+ * No other client would try to access the backend
+ *
+ */
+builder.Services.AddCors(options => {
+        options.AddPolicy(ELECTRON_CORS, policy => {
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+        });
+    });
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddSqlite<AppDbContext>(connectionString);
@@ -11,7 +32,6 @@ builder.Services.AddSqlite<AppDbContext>(connectionString);
 var app = builder.Build();
 
 if(app.Environment.IsDevelopment()) {
-    
     app.UseSwagger();
     app.UseSwaggerUI();
 
@@ -22,6 +42,8 @@ if(app.Environment.IsDevelopment()) {
     context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
+
+app.UseCors(ELECTRON_CORS);
 
 app.MapResidentEndpoints();
 app.MapTransactionEndpoints();
