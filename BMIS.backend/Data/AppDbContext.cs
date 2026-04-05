c18b9724582e2e1736e16c6f462607ef399d0824
louJ-dev <jericfactor@gmail.com>
2026-04-05 15:52:20 +0800

    i dunno anymore

---
+++ BMIS.backend/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using BMIS.Models.Entities;
+
+public class AppDbContext : DbContext {
+    public DbSet<Resident> Residents { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
