a5e67e58619cbfcf758671a44c1991b2032854eb
louJ-dev <jericfactor@gmail.com>
2026-05-05 15:55:45 +0800

    added jaro winkler distance implementation

--- BMIS.backend/Endpoints/ResidentEndpoints.cs
+++ BMIS.backend/Endpoints/ResidentEndpoints.cs
@@ -101,7 +101,73 @@ public static class ResidentEndpoints {
     }
 
 
+    private static double JaroWinklerSim(string a, string b) {
+        a = a.Trim(new char[]{',', ' '}).ToLower();    
+        b = b.Trim(new char[]{',', ' '}).ToLower();    
 
+        int searchDis = Math.Min((int)Math.Floor(Math.Max(a.Length, b.Length) / 2.0) - 1, 4);
+        
+        bool[] aflag = new bool[a.Length];
+        bool[] bflag = new bool[b.Length];
+        
+        double common = 0;
+        double transp = 0;
+
+        int i = 0;
+        while(i < a.Length){
+            int lowLim = Math.Max(0, i - searchDis);
+            int uppLim = Math.Min(i + searchDis + 1, b.Length);
+            for(int k = lowLim; k < uppLim; k++) {
+                if(!bflag[k] && a[i] == b[k]) {
+                    common += 1;
+                    aflag[i] = true;
+                    bflag[k] = true;
+                    break;
+                }
+            }
+
+            i++;
+        }
+
+        if(common == 0) {
+            return 0;
+        }
+
+        int l = 0;
+        for(int j = 0; j < a.Length; j++) {
+            if(aflag[j]) {
+                while(bflag[l] == false) {
+                    l++;
+                }
+
+                transp += a[j] != b[l] ? 0.5 : 0;
+                l++;
+            }
+        }
+
+
+        double f1 = common / a.Length;
+        double f2 = common / b.Length;
+        double f3 = (common - transp) / common;
+        double jaro = (f1 + f2 + f3) / 3.0;
+        
+        if(jaro < 0.7) {
+            return jaro ;
+        }
+
+        double pref = 0;
+        int maxPref = Math.Min(Math.Min(a.Length, b.Length), 4);
+
+        i = 0;
+        while(i < maxPref && a[i] == b[i]) {
+            pref++;
+            i++;
+        }
+
+        double winkler = jaro + (pref * 0.1 * (1.0 - jaro));  
+
+        return winkler ;
+    }
 
 
 
