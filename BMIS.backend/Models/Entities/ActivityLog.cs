fe0c918c4a4b70f182ce9a1683a90bbee56916e2
louJ-dev <jericfactor@gmail.com>
2026-05-15 04:12:28 +0800

    added activity log services

--- BMIS.backend/Models/Entities/ActivityLog.cs
+++ BMIS.backend/Models/Entities/ActivityLog.cs
@@ -3,5 +3,11 @@ namespace BMIS.Models.Entities;
 public class ActivityLog {
     public int Id { get; set; }
     public int HandlerId { get; set; }
-    public string Log { get; set; }
+    public string Message { get; set; }
+
+
+    public ActivityLog(int handlerId, string message) {
+        this.HandlerId = handlerId;
+        this.Message = message;
+    }
 }
