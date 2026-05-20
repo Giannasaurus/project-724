4b2461e5064febd5fe34c780240b2da6589f551b
giannasaurus <gianzcantor@gmail.com>
2026-05-20 16:45:19 +0800

    Frontend start

---
+++ BMIS.frontend/preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    getApiPort: () => ipcRenderer.invoke('get-api-port'),
+    getData: (endpoint) => ipcRenderer.invoke('get-data', endpoint),
+    postData: (endpoint, body) => ipcRenderer.invoke('post-data', endpoint, body),
+    updateData: (endpoint, body) => ipcRenderer.invoke('update-data', endpoint, body),
+    deleteData: (endpoint, id) => ipcRenderer.invoke('delete-data', endpoint, id),
+    readResidentsExcel: () => ipcRenderer.invoke('read-residents-excel'),
+    saveWordDocument: (html, fileName, context) => ipcRenderer.invoke('save-word-document', html, fileName, context),
+    exportAppBackup: (localData) => ipcRenderer.invoke('export-app-backup', localData),
+    readAppBackup: () => ipcRenderer.invoke('read-app-backup'),
+    restoreBackupDatabase: (database) => ipcRenderer.invoke('restore-backup-database', database),
+})
