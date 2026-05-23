75e0511c0c35b97d833b104f5b44ab4f5e9bfe1a
louJ-dev <jericfactor@gmail.com>
2026-05-23 14:35:10 +0800

    fixed user's name and password not settable

--- BMIS.backend/Models/Entities/User.cs
+++ BMIS.backend/Models/Entities/User.cs
@@ -2,7 +2,7 @@ namespace BMIS.Models.Entities;
 
 public class User {
     public Guid Id { get; set; }
-    public required string UserName { get; }
-    public required string Password { get; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
     public required bool IsActive { get; set; } 
 }
